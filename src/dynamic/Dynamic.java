9e7b3f29b82f04df9377f3f6598746b7cf32e653
tagrohan <tagrohan@gmail.com>
2022-01-23 23:00:37 +0530

    fab added

--- src/dynamic/Dynamic.java
+++ src/dynamic/Dynamic.java
@@ -5,4 +5,9 @@ public class Dynamic {
     public static void main(String[] args) {
         System.out.println("sp- start from here");
     }
+
+    // using dp
+    public static int fab(int range, int[] dp){
+        return -1;
+    }
 }
