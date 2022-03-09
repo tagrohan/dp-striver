99d911ef1b78aece7ddaa68718626dc0ff56e6a1
tagrohan <tagrohan@gmail.com>
2022-03-10 00:19:53 +0530

    sortKth added

--- src/hashmap_heap/HeapMain.java
+++ src/hashmap_heap/HeapMain.java
@@ -1,13 +1,31 @@
 package hashmap_heap;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class HeapMain {
    public static void main(String[] args) {
-      System.out.println(kthLargest(List.of(5, 4, 3, 2, 7, 6, 8, 5), 3));
+      int[] arr = new int[]{6, 5, 3, 2, 8, 10, 9};
+      System.out.println(Arrays.toString(sortKSorted(arr, 3)));
+   }
+
+   private static int[] sortKSorted(int[] arr, int k) {
+      Queue<Integer> queue = new PriorityQueue<>();
+      int[] sortedArray = new int[arr.length];
+      int i = 0;
+      for (int var : arr) {
+         queue.add(var);
+         if (queue.size() > k) {
+            sortedArray[i] = queue.remove();
+            i += 1;
+         }
+      }
+      if (!queue.isEmpty()) {
+         while (!queue.isEmpty()) {
+            sortedArray[i] = queue.remove();
+            i += 1;
+         }
+      }
+      return sortedArray;
    }
 
    private static int kthLargest(List<Integer> list, int k) {
