6d4a85ee1d6ca3d3a1ac19c36ca0fc0d728560a0
tagrohan <tagrohan@gmail.com>
2022-03-10 00:03:38 +0530

    kthLargest and smallest modifies

--- src/hashmap_heap/HeapMain.java
+++ src/hashmap_heap/HeapMain.java
@@ -7,14 +7,27 @@ import java.util.Queue;
 
 public class HeapMain {
    public static void main(String[] args) {
-      System.out.println(kthSmallest(List.of(5, 4, 3, 2, 7, 6, 8, 5), 3));
+      System.out.println(kthLargest(List.of(5, 4, 3, 2, 7, 6, 8, 5), 3));
    }
 
+   private static int kthLargest(List<Integer> list, int k) {
+//      System.out.println(kthLargest(List.of(5, 4, 3, 2, 7, 6, 8, 5), 3));
+      Queue<Integer> queue = new PriorityQueue<>();
+      list.forEach(ele -> {
+         queue.add(ele);
+         if (queue.size() > k) queue.remove();
+      });
+      return queue.isEmpty() ? -1 : queue.peek();
+
+   }
+
+
    private static int kthSmallest(List<Integer> list, int k) {
+//      System.out.println(kthSmallest(List.of(5, 4, 3, 2, 7, 6, 8, 5), 3));
       Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
       list.forEach(ele -> {
-         if (queue.size() < k) queue.add(ele);
-         else queue.poll();
+         queue.add(ele);
+         if (queue.size() > k) queue.remove();
       });
       return queue.isEmpty() ? -1 : queue.peek();
    }
