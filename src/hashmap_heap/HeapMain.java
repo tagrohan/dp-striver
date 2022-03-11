e110ca425d1b144944f6383f05450a387d5dd015
tagrohan <tagrohan@gmail.com>
2022-03-11 13:06:13 +0530

    kFrequentElement added

--- src/hashmap_heap/HeapMain.java
+++ src/hashmap_heap/HeapMain.java
@@ -3,6 +3,36 @@ package hashmap_heap;
 import java.util.*;
 
 public class HeapMain {
+
+   public static void main(String[] args) {
+      kFrequentNumber(new int[]{1, 1, 1, 3, 2, 2, 24}, 2);
+   }
+
+   // top k frequent element ex for k = 2 in [1,1,1,3,2,2,4] is 1,2
+   private static void kFrequentNumber(int[] arr, int k) {
+//      kFrequentNumber(new int[]{1, 1, 1, 3, 2, 2, 24}, 2);
+      Map<Integer, Integer> map = gettingFrequency(arr);
+      Queue<Pair> queue = new PriorityQueue<>();
+      map.forEach((key, frequency) -> {
+         queue.add(new Pair(key, frequency));
+         if (queue.size() > k) queue.poll();
+      });
+      queue.forEach(el -> System.out.println(el.index + " "));
+   }
+
+   private static Map<Integer, Integer> gettingFrequency(int[] arr) {
+      Map<Integer, Integer> map = new HashMap<>();
+//      for (int ele : arr) {
+//         if (map.containsKey(ele)) {
+//            map.put(ele, map.get(ele) + 1);
+//         } else map.put(ele, 1);
+//      }
+      // both working fine
+      for (int ele : arr) map.put(ele, map.containsKey(ele) ? map.get(ele) + 1 : 1);
+      return map;
+   }
+
+
    // todo imp
    static class Pair implements Comparable<Pair> {
 
@@ -20,11 +50,8 @@ public class HeapMain {
       }
    }
 
-   public static void main(String[] args) {
-      kClosestNumber(new int[]{5, 6, 10, 20, 30, 7, 8, 9, 10, 11, 12}, 3, 7);
-   }
-
    private static void kClosestNumber(int[] arr, int k, int num) {
+//      kClosestNumber(new int[]{5, 6, 10, 20, 30, 7, 8, 9, 10, 11, 12}, 3, 7);
       int[] closest = new int[k];
       Queue<Pair> queue = new PriorityQueue<>(Collections.reverseOrder());
       for (int i = 0; i < arr.length; i++) {
