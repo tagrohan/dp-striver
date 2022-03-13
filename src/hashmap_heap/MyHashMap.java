e79d9e6895c5f88140a79d1cfb6ff39611b6a2ca
tagrohan <tagrohan@gmail.com>
2022-03-13 11:24:37 +0530

    printCommonElementV1 added

--- src/hashmap_heap/MyHashMap.java
+++ src/hashmap_heap/MyHashMap.java
@@ -1,15 +1,29 @@
 package hashmap_heap;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class MyHashMap {
    public static void main(String[] args) {
-      printCommonElement(new int[]{1, 1, 2, 2, 3, 3, 4}, new int[]{1, 1, 1, 2, 2, 4, 5});
+      printCommonElementV1(new int[]{1, 1, 2, 2, 3, 3, 4}, new int[]{1, 1, 1, 2, 2, 4, 5});
    }
 
-   private static void printCommonElement(int[] arr1, int[] arr2) {
-//      printCommonElement(new int[]{1, 1, 2, 2, 3, 3, 4}, new int[]{1, 1, 1, 2, 2, 4, 5});
+   private static void printCommonElementV1(int[] arr1, int[] arr2) {
+//      printCommonElementV1(new int[]{1, 1, 2, 2, 3, 3, 4}, new int[]{1, 1, 1, 2, 2, 4, 5});
+      Set<Integer> set = new HashSet<>();
+      for (int val : arr1) set.add(val);
+      for (int val : arr2) {
+         if (set.contains(val)) {
+            System.out.print(val);
+            set.remove(val);
+         }
+      }
+   }
+
+   private static void printCommonElementV2(int[] arr1, int[] arr2) {
+//      printCommonElementV2(new int[]{1, 1, 2, 2, 3, 3, 4}, new int[]{1, 1, 1, 2, 2, 4, 5});
       Map<Integer, Integer> map = new HashMap<>();
       for (int val : arr1) map.put(val, map.containsKey(val) ? map.get(val) + 1 : 1);
       for (int val : arr2)
