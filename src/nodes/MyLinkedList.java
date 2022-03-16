0ca1cfdce5c670f526af45af4fa6999d31741454
tagrohan <tagrohan@gmail.com>
2022-03-16 23:55:10 +0530

    print LL added

--- src/nodes/MyLinkedList.java
+++ src/nodes/MyLinkedList.java
@@ -3,6 +3,14 @@ package nodes;
 public class MyLinkedList {
 
    public static void main(String[] args) {
+      LinkedNode root = new LinkedNode(1, null);
+      LinkedNode r2 = new LinkedNode(2, null);
+      LinkedNode r3 = new LinkedNode(3, null);
+      LinkedNode r4 = new LinkedNode(4, null);
+      root.next = r2;
+      r2.next = r3;
+      r3.next = r4;
+      printList(addLast(root, 5));
 
    }
 
@@ -13,6 +21,13 @@ public class MyLinkedList {
       }
       temp.next = new LinkedNode(data, null);
 
-      return temp;
+      return root;
+   }
+
+   private static void printList(LinkedNode root) {
+      while (root != null) {
+         System.out.print(root.data + " ");
+         root = root.next;
+      }
    }
 }
