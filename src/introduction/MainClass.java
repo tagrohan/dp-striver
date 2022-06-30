1e53e207fb1e3f652b2e089632752de2e9d07b27
Tagrohan <tagrohan@gmail.com>
2022-06-30 19:52:18 +0530

    decInc added

--- src/introduction/MainClass.java
+++ src/introduction/MainClass.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("working..");
-
-        System.out.println(Arrays.toString(findAllIndex(new int[]{1, 4, 1, 2, 3, 4, 4, 5, 5, 6}, 0, 0, 8)));
+        printDeCInc(5);
     }
 
     private static int[] findAllIndex(int[] arr, int index, int noOfElementFound, int element) {
+//        System.out.println(Arrays.toString(findAllIndex(new int[]{1, 4, 1, 2, 3, 4, 4, 5, 5, 6}, 0, 0, 4)));
         if (arr.length == index) return new int[noOfElementFound];
         if (arr[index] == element) noOfElementFound += 1;
 
@@ -18,5 +18,12 @@ public class MainClass {
             indexes[noOfElementFound - 1] = index;
         }
         return indexes;
+    }
+
+    private static void printDeCInc(int n) {
+        if (n == 0) return;
+        System.out.println(n);
+        printDeCInc(n - 1);
+        System.out.println(n);
     }
 }
