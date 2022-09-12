fa2779d8d9c188e82d133ebdb0896381acd2d96a
tagrohan <tagrohan@gmail.com>
2022-09-12 23:50:37 +0530

    BFS in graph added

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,8 +1,42 @@
 package graph;
 
+import java.util.*;
+
 public class Graph {
 
     public static void main(String[] args) {
+        List<List<Integer>> adL = new ArrayList<>();
+        for (int i = 0; i < 9; i++) adL.add(new ArrayList<>());
 
+        adL.add(1, List.of(2, 6));
+        adL.add(2, List.of(1, 3, 4));
+        adL.add(3, List.of(2));
+        adL.add(4, List.of(2, 5));
+        adL.add(5, List.of(4, 7));
+        adL.add(6, List.of(1, 7, 8));
+        adL.add(7, List.of(5, 6, 8));
+        adL.add(8, List.of(6));
+        System.out.println(Arrays.toString(BFS(adL).toArray()));
+    }
+
+    private static List<Integer> BFS(List<List<Integer>> adL) { // adL adjecency list
+        Queue<Integer> queue = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[adL.size()];
+        queue.add(1);
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int data = queue.poll();
+            list.add(data);
+
+            for (int val : adL.get(data)) {
+                if (!visited[val]) {
+                    queue.add(val);
+                    visited[val] = true;
+                }
+            }
+        }
+        return list;
     }
 }
