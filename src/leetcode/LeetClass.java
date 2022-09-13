e62cfc3c52c7d731321052fbe7fc424fe632fad8
tagrohan <tagrohan@gmail.com>
2022-09-13 22:24:57 +0530

    tried post using stack

--- src/leetcode/LeetClass.java
+++ src/leetcode/LeetClass.java
@@ -1,58 +1,98 @@
 package leetcode;
 
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class LeetClass {
 
-    private static class Node {
-        int val;
-        Node next;
+   private static class Node {
+      int val;
+      Node next;
 
-        public Node(int val, Node next) {
-            this.val = val;
-            this.next = next;
-        }
+      public Node(int val, Node next) {
+         this.val = val;
+         this.next = next;
+      }
 
-        public Node(int val) {
-            this.val = val;
-        }
-    }
+      public Node(int val) {
+         this.val = val;
+      }
+   }
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
+   private static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
 
-    private static void num() {
+      public TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+   }
 
-    }
+   public static List<Integer> postorderTraversal(TreeNode root) {
+      if (root == null) return List.of();
+      Stack<TreeNode> stack = new Stack<>();
+      List<Integer> list = new ArrayList<>();
+      stack.push(root);
+
+      while (!stack.isEmpty()) {
+
+         TreeNode temp = stack.peek();
+         if (temp.right == null && temp.left == null) {
+            list.add(temp.val);
+
+            stack.pop();
+            continue;
+         }
+         if (temp.right != null) stack.push(temp.right);
+         if (temp.left != null) stack.push(temp.left);
+      }
+      return list;
+   }
+
+   public static void main(String[] args) {
+      TreeNode node = new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null, null), null));
+      System.out.println(Arrays.toString(postorderTraversal(node).toArray()));
+
+   }
+
+   private static void num() {
+
+   }
 
 //    private static boolean isPalindromeInON(Node head){
 //        Node head = new Node(1,new Node(2,new Node(2,new Node(1))));
 //        System.out.println(isPalindromeInON(head));
 //    }
 
-    private static boolean isValidBrackets(String str) {
+   private static boolean isValidBrackets(String str) {
 
 
-        System.out.println(isValidBrackets("]"));
+      System.out.println(isValidBrackets("]"));
 
-        Stack<Character> stack = new Stack<>();
+      Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch == '{' || ch == '[' || ch == '(') stack.push(ch);
-            else if (ch == '}' || ch == ']' || ch == ')') {
-                if (stack.isEmpty() || stack.peek() != getOpeningBracket(ch)) return false; // what if input is "]"
-                else stack.pop();
-            }
-        }
-        return stack.isEmpty();
-    }
+      for (int i = 0; i < str.length(); i++) {
+         char ch = str.charAt(i);
+         if (ch == '{' || ch == '[' || ch == '(') stack.push(ch);
+         else if (ch == '}' || ch == ']' || ch == ')') {
+            if (stack.isEmpty() || stack.peek() != getOpeningBracket(ch)) return false; // what if input is "]"
+            else stack.pop();
+         }
+      }
+      return stack.isEmpty();
+   }
 
-    private static char getOpeningBracket(char ch) {
-        if (ch == '}') return '{';
-        if (ch == ']') return '[';
-        return '(';
-    }
+   private static char getOpeningBracket(char ch) {
+      if (ch == '}') return '{';
+      if (ch == ']') return '[';
+      return '(';
+   }
 
 }
